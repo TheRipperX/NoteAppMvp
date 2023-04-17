735f812d3d0061ed441eef2ca8d6f27bb05db866
TheRipperX <ripper.cs-ret@hotmail.com>
2023-04-17 16:56:47 +0330

    add fragment note and make fragment layout

---
+++ app/src/main/java/com/example/mvp_noteapp/ui/add/NoteFragment.kt
@@ -0,0 +1,33 @@
+package com.example.mvp_noteapp.ui.add
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.mvp_noteapp.databinding.FragmentNoteBinding
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+
+class NoteFragment : BottomSheetDialogFragment() {
+
+    private lateinit var binding: FragmentNoteBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = FragmentNoteBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        main()
+    }
+
+    private fun main() {
+
+        binding.apply {
+
+        }
+    }
+}
--- app/src/main/java/com/example/mvp_noteapp/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/mvp_noteapp/ui/main/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.mvp_noteapp
+package com.example.mvp_noteapp.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
